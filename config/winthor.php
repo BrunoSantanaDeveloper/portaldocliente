09a8932c1d5c92c1258287743f4f305765b4ad41
Las-do-Brasil <git.las@lasdobrasil.com.br>
2022-05-11 09:54:51 -0300

    Update winthor.php
    Atualização para api da producao

--- config/winthor.php
+++ config/winthor.php
@@ -4,7 +4,7 @@ return [
     'name' => 'Winthor',
     'user' => 'portal.ar',
     'pass' => 'Keypass',
-    'winthor_url' => 'https://apidevlas.com:9493/',
+    'winthor_url' => 'https://apidevlas.com:9495/',
     'token' => 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImxlYW5kcm8iLCJyb2xlIjoibWFuYWdlciIsIm5iZiI6MTYzNTI3Mjg1OSwiZXhwIjoxNjY2ODA4ODU5LCJpYXQiOjE2MzUyNzI4NTl9.Ifss_kAj-aYmoYe-v756JPUkfAB0Su5uIthBBK_KScFIWQMANkronPUcPXKvkrH-U_2Uzug_pSv8i4nWaJ2qtA',
     'token_expires' => ''
 
