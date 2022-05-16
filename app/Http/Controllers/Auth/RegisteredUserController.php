33bf0eb28dccb7b1a701a21a42b632332e19ba92
Bruno Santana <bsantana.it@gmail.com>
2022-05-16 17:39:38 -0300

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,14 +42,14 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
  */
-        dd($request);
+
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
+        dd($user);
         //dd(DB::getQueryLog());
 
 
