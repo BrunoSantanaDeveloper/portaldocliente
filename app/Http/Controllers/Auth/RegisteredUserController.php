69924374e06c5e41cdf4a9ed220103b38a87a5c5
Bruno Santana <bsantana.it@gmail.com>
2022-05-16 17:27:25 -0300

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,13 +36,13 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
 
-        $request->validate([
+       /*  $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        dd($request);
+        dd($request); */
 
         $user = User::create([
             'name' => $request->name,
