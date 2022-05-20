b26f06a1e1a32e65f09ba44b4130631d30bd9388
CesarCcoyori <cesarccoyori8@gmail.com>
2022-05-20 11:51:32 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Portal_Do_Cliente</title>
+        <title>Portal do Cliente</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -30,41 +30,61 @@
                 </div>
 
                 <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg">
-                    <div class="grid grid-cols-1 md:grid-cols-1">
+                    <div class="grid grid-cols-1 md:grid-cols-1 text-center">
                         <div class="ml-4 text-lg leading-7 font-semibold">Portal do Cliente</div>
-
                     </div>
                     <div class="grid grid-cols-1 md:grid-cols-2">
-
                         @if (Route::has('login'))
-                            
-                                @auth
+                            @auth
                                 <div class="flex items-center">
                                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                                     <div class="ml-4 text-lg leading-7 font-semibold"><a href="{{ url('/dashboard') }}" class="underline text-gray-900 dark:text-white">Dashboard</a></div>
                                 </div>
+
                                 @else
 
                                 <div class="p-6 border-t border-gray-200 dark:border-gray-700">
                                     <div class="flex items-center">
-                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-black"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
-                                        <div class="ml-4 text-lg leading-7 font-semibold"><a href="{{ route('login') }}" class="underline text-black">Acessar Painel</a></div>
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-black"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path>
+                                        </svg>
+
+                                        <div class="ml-4 text-lg leading-7 font-semibold"><a href="{{ route('login') }}" class="underline text-black">Acessar Painel</a>
+                                        </div>
                                     </div>
                                 </div>
 
-                                    @if (Route::has('register'))
+                                @if (Route::has('register'))
                                     <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
                                         <div class="flex items-center">
-                                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-black"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                            <div class="ml-4 text-lg leading-7 font-semibold "><a href="{{ route('register') }}" class="underline text-black">Criar conta</a></div>
+                                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-black"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                            </svg>
+
+                                            <div class="ml-4 text-lg leading-7 font-semibold "><a href="{{ route('register') }}" class="underline text-black">Criar conta</a>
+                                            </div>
                                         </div>
                                     </div>
-                                    @endif
-                                @endauth
-                           
+                                @endif
+                            @endauth
                         @endif
+                    </div>
 
-                       
+                    <div class="grid grid-cols-1 md:grid-cols-2">   
+                        <div class="p-6 border-t border-gray-200 dark:border-gray-700">
+                            <div class="flex items-center">
+                                <img src="{{ asset('storage/images/logotipolas.svg') }}" alt="logo" class="d-inline"  style="height:1em">
+                                <a href="https://las.app.br" class="ml-1 underline">
+                                    las.app.br
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
+                            <div class="flex items-center">
+                                <div class="ml-4 text-center text-sm text-white-500 sm:text-right sm:ml-0">
+                                    Portal do Cliente V1.0
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -72,13 +92,11 @@
                     <div class="text-center text-sm text-black-500 sm:text-left">
                         <div class="flex items-center">
                             <img src="{{ asset('storage/images/logotipolas.svg') }}" alt="logo" class="d-inline"  style="height:1em">
-
                             <a href="https://las.app.br" class="ml-1 underline">
                                 las.app.br
                             </a>
                         </div>
                     </div>
-
                     <div class="ml-4 text-center text-sm text-white-500 sm:text-right sm:ml-0">
                         Portal do Cliente V1.0
                     </div>
