5e3c5941f470e82404ebe82403df5544fdc95ed8
CesarCcoyori <cesarccoyori8@gmail.com>
2022-05-20 16:48:48 -0300

    Title && Icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name') }}</title>
+        {{-- <title>{{ config('app.name') }}</title> --}}
+        <title>Portal do Cliente</title>
+
+        <!-- favicon -->
+        <link rel="icon" type="image/x-icon" href="http://portaldocliente.las.app.br/storage/images/Logo_favicon_LAS.png">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
