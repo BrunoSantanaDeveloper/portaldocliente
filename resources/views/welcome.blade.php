44ad4e31bd2186b8f6d1dbe13919e37b280359b5
Cesar <cesarccoyori8@gmail.com>
2022-06-10 12:07:25 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,10 +38,15 @@
                             <p>Portal do Cliente</p>
                         </div>
                     </div>
-                    
+
                     <div class="grid grid-cols-1 md:grid-cols-2">
-                        @if
+                        @if (Route::has('login'))
                             @auth
+                                <div class="flex items-center">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+                                    <div class="ml-4 text-lg leading-7 font-semibold"><a href="{{ url('/dashboard') }}" class="underline text-gray-900 dark:text-white">Dashboard</a></div>
+                                </div>
+                                @else
                                 <div class="p-6 border-t border-gray-200 dark:border-gray-700">
                                     <div class="flex items-center">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
