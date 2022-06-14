ce8179c4d3df4946635f971211591dd5b050884b
Las-do-Brasil <git.las@lasdobrasil.com.br>
2022-06-14 16:40:31 -0300

    Update Orders.php

--- app/Http/Controllers/Orders.php
+++ app/Http/Controllers/Orders.php
@@ -36,7 +36,6 @@ class Orders extends Controller
 
                 $message->from('noreply@portaldocliente.las.app.br', 'LOGÍSTICA - LAS');
                 $message->sender('noreply@portaldocliente.las.app.br', 'LOGÍSTICA - LAS');
-                $message->to('ti@lasdobrasil.com.br', 'TI - LAS');
                 $message->cc('logistica@lasdobrasil.com.br', 'LOGÍSTICA - LAS');
                 $message->cc('estoque@lasdobrasil.com.br', 'LOGÍSTICA - Estoque');
                 //$message->cc($dataMail['emitente'], $dataMail['emitenteEmail'] = null);
